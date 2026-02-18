c554a90c55c180e2a96e02106a94601f5597dcfa
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-02-18 16:10:02 +0000

    Adicionados simbulos

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -11,10 +11,14 @@ namespace VariousTypes
             int numerosseguidos = 123456;
             int numerosparatras = 654321;
             string nome = "Daniel Professor";
+            char emoji = '\u260F';
+            char emoji2 = '\u2713';
 
             Console.WriteLine(numerosseguidos);
             Console.WriteLine(numerosparatras);
             Console.WriteLine(nome);
+            Console.WriteLine(emoji);
+            Console.WriteLine(emoji2);
         }
     }
 }
