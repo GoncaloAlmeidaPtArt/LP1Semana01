0d25781d47979d0a42b48a0bfff20a3f3e9131ca
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-02-18 16:15:20 +0000

    Adicionados os booleans e floats

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -13,12 +13,18 @@ namespace VariousTypes
             string nome = "Daniel Professor";
             char emoji = '\u260F';
             char emoji2 = '\u2713';
+            float numerozinho = 4.2f;
+            bool certo = true;
+            bool errado = false;
 
             Console.WriteLine(numerosseguidos);
             Console.WriteLine(numerosparatras);
             Console.WriteLine(nome);
             Console.WriteLine(emoji);
             Console.WriteLine(emoji2);
+            Console.WriteLine(certo);
+            Console.WriteLine(errado);
+            Console.WriteLine(numerozinho);
         }
     }
 }
