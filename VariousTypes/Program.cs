73c199a508d6809fd78ff5ce391ddfd046db8609
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-02-18 16:00:00 +0000

    Adicionado o projeto das variaveis

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            int numerosseguidos = 123456;
+            int numerosparatras = 654321;
+            string nome = "Daniel Professor";
+
+            Console.WriteLine(numerosseguidos);
+            Console.WriteLine(numerosparatras);
+            Console.WriteLine(nome);
+        }
+    }
+}
