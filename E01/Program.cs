d3c977663fee65124dda8a3a44ed0edc7e26a4c9
Gonçalo <goncaloalmeida.artwork@gmail.com>
2026-02-19 16:13:16 +0000

    Adicionado o codigo para mostrar o simbulo do R

--- E01/Program.cs
+++ E01/Program.cs
@@ -6,7 +6,9 @@ namespace E01
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+
+            Console.WriteLine("\\u00AE");
+            
         }
     }
 }
